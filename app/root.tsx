38dfb7e645598b78c1536ef173788f3fdf18de11
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-11-18 22:04:10 -0400

    asd

--- app/root.tsx
+++ app/root.tsx
@@ -1,26 +1,70 @@
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
   Meta,
-  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import styles from "./tailwind.css";
+import nprogressCss from "./nprogress.css";
+
+import Layout from "~/components/Layout";
+import type { DataFunctionArgs } from "@remix-run/node";
+import { useRouteProgressBar } from "./utils/useRouteProgressBar";
+
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: styles },
+  { rel: "stylesheet", href: nprogressCss },
+];
+
+// export const meta: V2_MetaFunction = () => [
+//   {
+//     name: "viewport",
+//     content: "initial-scale=1, viewport-fit=cover",
+//     "viewport-fit": "cover",
+//   },
+// ];
+
+export async function loader({ request, params }: DataFunctionArgs) {
+  const userAgent = request.headers.get("User-Agent");
+  const isMobile = userAgent?.match(
+    /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+  );
+
+  return { isMobile };
+}
 
 export default function App() {
+  const { isMobile } = useLoaderData<typeof loader>();
+  useRouteProgressBar(!isMobile);
+
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className="flex flex-col min-h-full overflow-x-hidden relative"
+    >
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="initial-scale=1, viewport-fit=cover, width=device-width"
+          viewport-fit="cover"
+        />
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
+      <body className="h-[100dvh] relative">
+        <Layout />
         <ScrollRestoration />
-        <LiveReload />
         <Scripts />
+        <LiveReload />
+        <div className="absolute text-white text-xs top-[-500px]">
+          {"A".repeat(2048)}
+        </div>
       </body>
     </html>
   );
